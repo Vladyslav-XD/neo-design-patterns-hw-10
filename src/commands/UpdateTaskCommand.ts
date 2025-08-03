1bc79140e8f4b6e12c982a261526b630930028cf
Vladyslav-XD <vladphilon@gmail.com>
2025-08-03 10:10:44 +0300

    add fix to undo

--- src/commands/UpdateTaskCommand.ts
+++ src/commands/UpdateTaskCommand.ts
@@ -3,7 +3,7 @@ import { TaskList } from '../models/TaskList';
 import { Task } from '../models/Task';
 
 export class UpdateTaskCommand extends AbstractCommand {
-    private previousTask?: Partial<Task>;
+    private previousTask?: Task;
 
     constructor(
         private taskList: TaskList,
@@ -14,9 +14,10 @@ export class UpdateTaskCommand extends AbstractCommand {
     }
 
     execute(): void {
-        const task = this.taskList.updateTask(this.taskId, this.updates);
-        if (task) {
-            this.previousTask = this.invertUpdates(this.updates, task);
+        const original = this.taskList.getAllTasks().find(t => t.id === this.taskId);
+        if (original) {
+            this.previousTask = { ...original };
+            this.taskList.updateTask(this.taskId, this.updates);
         }
     }
 
@@ -25,36 +26,4 @@ export class UpdateTaskCommand extends AbstractCommand {
             this.taskList.updateTask(this.taskId, this.previousTask);
         }
     }
-
-    private invertUpdates(
-        updates: Partial<Task>,
-        current: Task
-    ): Partial<Task> {
-        const inverted: Partial<Task> = {};
-
-        for (const key of Object.keys(updates) as (keyof Task)[]) {
-            switch (key) {
-                case 'id':
-                case 'title':
-                case 'description':
-                case 'priority':
-                    inverted[key] = current[key] as 'low' | 'medium' | 'high';
-                    break;
-                case 'completed':
-                    inverted[key] = current[key] as boolean;
-                    break;
-                case 'createdAt':
-                case 'dueDate':
-                    inverted[key] = current[key] as Date;
-                    break;
-                case 'tags':
-                    inverted[key] = current[key] as string[];
-                    break;
-            }
-        }
-
-        return inverted;
-    }
-
-
 }
